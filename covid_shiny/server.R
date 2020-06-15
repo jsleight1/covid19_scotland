f3893e67e408f2aa40af404233dddc3ddfe7ba6e
Jack Sleight <jack.sleight@fiosgenomics.com>
2020-06-15 21:56:34 +0100

    Update intro page

--- covid_shiny/server.R
+++ covid_shiny/server.R
@@ -24,13 +24,17 @@ shinyServer(function(input, output) {
     
     # Introduction
     output[["introduction_plot"]] <- renderPlotly({
-        daily_barplot(national_data[["Table 5 - Testing"]], x = "Date", y = "Positive") 
+        daily_barplot(
+            mutate(national_data[["Table 5 - Testing"]], `Confirmed Positive` = cumsum(Daily_Positive)), 
+            x = "Date", 
+            y = "`Confirmed Positive`"
+        ) 
     })
     output[["introduction_date"]] <- renderText({
         as.character(last(pull(national_data[["Table 8 - Deaths"]], Date)))
     })
     output[["introduction_cases"]] <- renderText({
-        last(pull(national_data[["Table 5 - Testing"]], Positive))
+        last(pull(mutate(national_data[["Table 5 - Testing"]], `Confirmed Positive` = cumsum(Daily_Positive)), `Confirmed Positive`))
     })
     output[["introduction_daily_cases"]] <- renderText({
         last(pull(national_data[["Table 5 - Testing"]], Daily_Positive))
@@ -40,8 +44,8 @@ shinyServer(function(input, output) {
     })
     output[["introduction_daily_deaths"]] <- renderText({
         df <- find_daily_increase(
-            national_data[["Table 8 - Deaths"]], 
-            "`Number of COVID-19 confirmed deaths registered to date`"
+            df = national_data[["Table 8 - Deaths"]], 
+            column = "`Number of COVID-19 confirmed deaths registered to date`"
         )
         last(pull(df, `Daily Change`))
     })
