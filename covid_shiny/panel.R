6f73f237be8817090bc258320ed7a9bbf6d1acda
Jack Sleight <jacksleight@hotmail.co.uk>
2021-01-21 15:37:28 +0000

    add removeUI function for plots

--- covid_shiny/panel.R
+++ covid_shiny/panel.R
@@ -13,9 +13,8 @@ panelUI <- function(id, message = "") {
             ),
             tabPanel(
                 h6("Plot"), 
-                # plotUI(ns("plot1")), 
                 actionButton(ns("addPlot"), "Add new plot panel"),
-                # actionButton(ns("removePlot"), "Remove plot panel"),
+                actionButton(ns("removePlot"), "Remove plot panel"),
                 tags$div(id = gsub(" ", "_", ns("newplot")))
             )
         )
@@ -46,14 +45,12 @@ panelServer <- function(id, table, x = "Date", roll_ave = TRUE) {
                     write_tsv(table, file)
                 }
             )
-            # plotServer("plot1", table = table, x = x, roll_ave = roll_ave)
             plots <- c()
             observeEvent(input[["addPlot"]], {
-                browser()
                 plotid <- paste0(gsub(" ", "_", ns("newplot")), input[["addPlot"]])
                 insertUI(
-                    selector = paste0("#", gsub(" ", "_",ns("newplot"))),
-                    ui = plotUI(ns(plotid))
+                    selector = paste0("#", gsub(" ", "_", ns("newplot"))),
+                    ui = tags$div(id = plotid, plotUI(ns(plotid)))
                 )
                 plotServer(
                     id = plotid, 
@@ -61,14 +58,14 @@ panelServer <- function(id, table, x = "Date", roll_ave = TRUE) {
                     x = x, 
                     roll_ave = roll_ave
                 )
-                plots <<- c(plotid, plots)
+                plots <<- c(plots, plotid)
             })
-            # observeEvent(input[["removePlot"]], {
-            #     removeUI(
-            #         selector = paste0("#", plots[length(plots)])
-            #     )
-            #     plots <<- plots[-length(plots)]
-            # })
+            observeEvent(input[["removePlot"]], {
+                removeUI(
+                    selector = paste0("#", plots[length(plots)])
+                )
+                plots <<- plots[-length(plots)]
+            })
         }
     ) 
 }
